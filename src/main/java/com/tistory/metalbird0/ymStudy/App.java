74f58dd9b1ee4fae7f46fbf26abff3fb52edefa6
ymJung <metalbird0@gmail.com>
2014-04-23 15:04:39 +0900

    euler algorithm solve

--- src/main/java/com/tistory/metalbird0/ymStudy/App.java
+++ src/main/java/com/tistory/metalbird0/ymStudy/App.java
@@ -6,5 +6,12 @@ package com.tistory.metalbird0.ymStudy;
  */
 public class App {
     public static void main(String[] args) {
+        int i = 0;
+     
+        System.out.println(--i + " " + --i + " " + --i + " " + --i);
+        
+        int j = 0;
+        
+        System.out.println(j++ + " " + j++ + " " + j++ + " " + j++);
     }
 }
